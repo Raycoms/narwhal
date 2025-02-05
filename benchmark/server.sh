e3063daac19d6744f33e1e28f829a6b876d923f9
Raycoms <ray.neiheiser@gmail.com>
2025-02-05 13:37:36 +0100

    use tmux properly

--- benchmark/server.sh
+++ benchmark/server.sh
@@ -134,10 +134,10 @@ echo "Starting Application: #${i}"
 ## Startup Narwahl
 
 # Startup Primaries
-./../target/release/node -vv run --keys ".node-${id}.json" --committee ".committee.json" --store ".db-${id}" --parameters ".parameters.json" primary |& tee "logs/primary-${id}.log" &
 
-# Startup Workers
-./../target/release/node -vv run --keys ".node-${id}.json" --committee ".committee.json" --store ".db-${id}-0" --parameters ".parameters.json" worker --id 0 |& tee "logs/worker-${id}.log" &
+tmux new -d -s "primary-${id}" "./../target/release/node -vv run --keys .node-${id}.json --committee .committee.json --store .db-${id} --parameters .parameters.json primary |& tee logs/primary-${id}.log"
+
+tmux new -d -s "worker-${id}" "./../target/release/node -vv run --keys .node-${id}.json --committee .committee.json --store .db-${id}-0 --parameters .parameters.json worker --id 0 |& tee logs/worker-${id}.log"
 
 sleep 90
 
